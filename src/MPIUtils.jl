5842bc4a60c0e10e06362a78666278446b631220
bmxam <maxime.bouyges@gmail.com>
2023-04-14 17:02:59 +0200

    fix for comm input

--- src/MPIUtils.jl
+++ src/MPIUtils.jl
@@ -7,16 +7,17 @@ Execute "action" on each processor but one at a time. For instance
 
 Involves an MPI.Barrier
 """
-macro one_at_a_time(action, comm = MPI.COMM_WORLD)
+macro one_at_a_time(action, comm = :(MPI.COMM_WORLD))
     return quote
-        rank = MPI.Comm_rank($comm)
-        nprocs = MPI.Comm_size($comm)
+        c = $(esc(comm))
+        rank = MPI.Comm_rank(c)
+        nprocs = MPI.Comm_size(c)
         for r = 0:nprocs-1
             if r == rank
                 print("[$r] ")
                 $(esc(action))
             end
-            MPI.Barrier($comm)
+            MPI.Barrier(c)
         end
     end
 end
@@ -25,21 +26,23 @@ end
 Execute `action` only on root processor, no MPI.Barrier
 """
 macro only_root(action, comm = MPI.COMM_WORLD)
-    return quote
-        rank = MPI.Comm_rank($comm)
+    q = quote
+        c = $(esc(comm))
+        rank = MPI.Comm_rank(c)
         if rank == 0
             print("[$rank] ")
             $(esc(action))
         end
     end
+    return q
 end
 
 """
 Execute `action` only on `rank` processor, no MPI.Barrier
 """
-macro only_proc(action, rank, comm = MPI.COMM_WORLD)
+macro only_proc(action, rank, comm = :(MPI.COMM_WORLD))
     return quote
-        my_rank = MPI.Comm_rank($comm)
+        my_rank = MPI.Comm_rank($(esc(comm)))
         if my_rank == $rank
             print("[$my_rank] ")
             $(esc(action))
